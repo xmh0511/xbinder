a897b8981c90d33e013a6ec257cb84c56e2f52ee
xmh0511 <970252187@qq.com>
2018-07-03 23:15:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 #include "xbinder.hpp"
-#include <string>
-void show(int a ,double b,long* ptr)
+void show(int& a, double& b)
 {
-    *ptr = 888888;
-    std::cout<<a<<","<<b<<std::endl;
+	std::cout << "a===" << a <<","<< "b===" << b << std::endl;
+	b = 1023.023;
 }
+
 struct Test
 {
-    void show(int a)
-    {
-         std::cout<<"show==="<<a<<"str===="<<m_str<<std::endl;
-    }
-    std::string m_str="default";
+	void show(int& a)
+	{
+		a = 88888;
+		m_abc = 233333;
+	}
+	int m_abc = 0;
 };
 int main()
 {
-    long* ptr = new long{};
-    auto lam = [](int a,float b){std::cout<<b<<std::endl;};
-    auto f = xmh::make_xbinder(lam,10,xmh::place_hold_1);
-    f(2.03);
-    auto f1 = xmh::make_xbinder(&show,xmh::place_hold_1,88.88,xmh::place_hold_2);
-    f1(1024,ptr);
-    std::cout<<*ptr<<std::endl;
-    auto f2 = xmh::make_xbinder(&Test::show,Test{},10);
-    f2();
-    return 0;
+
+	auto f0 = xmh::make_xbinder([](int& a) { std::cout << a << std::endl; a = 1024; }, xmh::place_hold_1);
+	int value0 = 10;
+	f0(value0);
+	std::cout << "value0===" << value0 << std::endl;
+	auto f1 = xmh::make_xbinder(&show, 10, xmh::place_hold_1);
+	double value1 = 20.1;
+	f1(value1);
+	std::cout << value1 << std::endl;
+	Test t;
+	auto f2 = xmh::make_xbinder(&Test::show, &t, xmh::place_hold_1);
+	int value2 = 1024;
+	f2(value2);
+	std::cout << value2 << std::endl;
+	std::cout << t.m_abc << std::endl;
+	std::cin.get();
+	return 0;
 }
